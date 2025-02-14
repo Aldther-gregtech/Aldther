064031efde1077f2ce8a2a8f55223841de873e23
Aldther-gregtech <vitalickblagov@yandex.ru>
2025-02-14 10:09:50 +0300

    Update Contract_Transform.py

--- Contract_Transform.py
+++ Contract_Transform.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from sqlalchemy import create_engine
 
-engine = create_engine('postgresql+psycopg2://postgres:root@localhost:5432/id_table')
+engine = create_engine('postgresql+psycopg2://')
 
 df = pd.read_sql_table('Contracts_TABLE', engine, schema='uploads')
 
